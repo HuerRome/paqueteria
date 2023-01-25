07e0997ad9e008d6278713be22fe0cf315c7c9e8
Romero Ana <97923571+HuerRome@users.noreply.github.com>
2023-01-25 15:50:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
+var button1 = document.querySelector(".left");
+var button2 = document.querySelector(".right");
+var carrusel = document.querySelector(".start .carousel > div");
+
+button1.addEventListener("click", function(){
+    img1.style.display="none";
+})
+
+button2.addEventListener("click", function(){
+    img2.style.display="none";
+})*/
+
+
+
+
+//menus desplegables
+let menu=document.querySelector(".container .nav-cel");
+let button= document.querySelector("nav > .menu button");
+
+button.addEventListener("click", function(){
+    menu.classList.toggle("active");
+})
+
+
+/* Declarando la función 'myFunc' */
+//function myFunc(){
+//}
+
+
+
+
