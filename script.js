d50182897eaf416c54ed15fdf450df0a69eb78e8
Romero Ana <97923571+HuerRome@users.noreply.github.com>
2023-01-26 02:40:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,17 +16,45 @@ button2.addEventListener("click", function(){
 
 
 //menus desplegables
-let menu=document.querySelector(".container .nav-cel");
-let button= document.querySelector("nav > .menu button");
+let menu = document.querySelector(".container .nav-cel");
+let button = document.querySelector("nav > .menu button");
 
 button.addEventListener("click", function(){
     menu.classList.toggle("active");
 })
 
 
-/* Declarando la función 'myFunc' */
-//function myFunc(){
-//}
+//go down button
+let down = document.querySelector(".slider-frame > div div");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 5000; // For Safari
+    document.documentElement.scrollTop = 5000; // For Chrome, Firefox, IE and Opera
+  }
+  
+  down.addEventListener("click",topFunction)
+
+//---------------------------------------Formularios-------------------------------------------------
+casillero = document.querySelector(".btn-casillero");
+formulario = document.querySelector(".casillero");
+
+casillero.addEventListener("click", function(){
+    formulario.classList.add("active");
+})
+
+
+casillero_cerrar = document.querySelector(".casillero > div > div > div:first-child button");
+formulario = document.querySelector(".casillero");
+
+casillero_cerrar.addEventListener("click", function(){
+    formulario.classList.remove("active");
+})
+
+
+
+
+
 
 
 
